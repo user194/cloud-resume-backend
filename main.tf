45f367a925b9ec3dc9c2d0b57262fd9d8bd7f2c7
satyanjana <satyan_jana@yahoo.com>
2026-07-09 12:16:27 +0000

    fix: │ Error: Error when reading or editing CloudRunV2Service "projects/thecloudresumechallenge/locations/us-central1/services/thecloudresumechallenge-counter-api": googleapi: Error 403: Permission run.services.get denied on resource projects/thecloudresumechallenge/locations/us-central1/services/thecloudresumechallenge-counter-api (or resource may not exist)."

--- main.tf
+++ main.tf
@@ -65,36 +65,3 @@ resource "google_cloud_run_v2_service_iam_member" "public_access" {
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
-
-# Automatically fetch project metadata (including the project number)
-data "google_project" "project" {
-  project_id = "thecloudresumechallenge"
-}
-
-# 4. Allow Cloud Datastore User role to Cloud Run service account
-resource "google_project_iam_member" "counter_api_firestore_access" {
-  project = "thecloudresumechallenge"
-  role    = "roles/datastore.user"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
-
-# 5. Cloud Run Admin Role to manage the counter service revisions
-resource "google_project_iam_member" "cloudbuild_run_admin" {
-  project = "thecloudresumechallenge"
-  role    = "roles/run.admin"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
-
-# 6. Service Account User Role to deploy the container with its runtime service account)
-resource "google_project_iam_member" "cloudbuild_sa_user" {
-  project = "thecloudresumechallenge"
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
-
-# 7. Artifact Registry Writer Role to push the buildpacks container image)
-resource "google_project_iam_member" "cloudbuild_registry_writer" {
-  project = "thecloudresumechallenge"
-  role    = "roles/artifactregistry.writer"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
